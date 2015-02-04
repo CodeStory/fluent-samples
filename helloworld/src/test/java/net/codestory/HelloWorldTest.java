5439d82d09a548d67752f78807495efa1bcd6793
David Gageot <david@gageot.net>
2015-02-04 16:11:44 +0100

    Improved Hello World

--- helloworld/src/test/java/net/codestory/HelloWorldTest.java
+++ helloworld/src/test/java/net/codestory/HelloWorldTest.java
@@ -1,22 +1,24 @@
 package net.codestory;
 
-import net.codestory.http.WebServer;
-import net.codestory.simplelenium.SeleniumTest;
-import org.junit.Test;
+import net.codestory.http.*;
+import net.codestory.simplelenium.*;
+
+import org.junit.*;
 
 public class HelloWorldTest extends SeleniumTest {
-  WebServer webServer = new WebServer().startOnRandomPort();
+	WebServer webServer = new WebServer().startOnRandomPort();
 
-  @Override
-  public String getDefaultBaseUrl() {
-    return "http://localhost:" + webServer.port();
-  }
+	@Override
+	public String getDefaultBaseUrl() {
+		return "http://localhost:" + webServer.port();
+	}
 
-  @Test
-  public void display_hello_world() {
-    goTo("/");
+	@Test
+	public void display_hello_world() {
+		goTo("/");
 
-    find(".jumbotron h1").should().contain("Hello, world!");
-    find(".jumbotron p").should().contain("This is a very basic sample of web application");
-  }
+		find(".jumbotron")
+				.find("h1").should().contain("Hello, world!")
+				.find("p").should().contain("This is a very basic sample of web application");
+	}
 }
